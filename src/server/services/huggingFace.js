354b8ba48fab9c83eb61dad4a883be3fee0d5e38
Michael Vandi <mikevee2013@gmail.com>
2023-11-06 17:07:54 -0800

    Added upload file, create repo, etc. to hugging face hubs

--- src/server/services/huggingFace.js
+++ src/server/services/huggingFace.js
@@ -1,4 +1,5 @@
-import { HfInference } from '@huggingface/inference'
+import { HfInference } from '@huggingface/inference';
+import { createRepo, uploadFile, deleteFiles} from "@huggingface/hub";
 
 class HuggingFace {
     // Looks like the access token is all we need
@@ -6,8 +7,8 @@ class HuggingFace {
         this.accessToken = accessToken;
         this.defaultOptions = defaultOptions;
 
-        const hf = new HfInference(this.accessToken);
-        this.hf = hf;
+        const inference = new HfInference(this.accessToken);
+        this.inference = inference;
     }
 
     /**
@@ -17,10 +18,68 @@ class HuggingFace {
      * @returns ?
      */
     async questionAnswering(model, inputs) {
-        return await this.hf.questionAnswer({
+        return await this.inference.questionAnswer({
             "model": model,
             "inputs": inputs,
         });
     }
+
+    /**
+     * @desc - Create a repository / folder in hugging face hubs
+     * @param {String} repoPath - The path to the repo
+     * @param {String} type - The type of repo. This is useful for "models"
+     * @returns 
+     */
+    async createRepo(repoPath, type) {
+        if (type == "model") {
+            return await createRepo({
+                repo: {type: type, name: repoPath},
+                credentials: {accessToken: this.accessToken}
+              });
+        } else {
+            return await createRepo({
+                repo: repoPath, // or {type: "model", name: "my-user/nlp-test"},
+                credentials: {accessToken: this.accessToken}
+              });
+        }
+        
+    }
+
+    /**
+     * @desc Upload a file, like a model to hugging face hub
+     * @param {String} repoPath 
+     * @param {String} filePath 
+     * @param {Blob} blob 
+     * @returns 
+     */
+    async uploadFile(repoPath, filePath, blob) {
+        return await uploadFile({
+            repo: repoPath,
+            credentials: {accessToken: this.accessToken},
+            // Can work with native File in browsers
+            file: {
+              path: filePath,  // Eg. "pytorch_model.bin",
+              content: blob
+            }
+          });
+    }
+
+    /**
+     * @desc Delete files in hugging face hub
+     * @param {String} type - Space | 
+     * @param {String} name - The path to the repo or space eg. my-user/my-space
+     * @param {Array} path - An array of strings representing the file paths to delete
+     * @returns 
+     */
+    async deleteFiles(type, name, paths) {
+        return await deleteFiles({
+            repo: {type: type, name: name}, // or "spaces/my-user/my-space"
+            credentials: {accessToken: this.accessToken},
+            paths: paths
+          });
+    }
+    
+
+    
 }
 module.exports = HuggingFace;
