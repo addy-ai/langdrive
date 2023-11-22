ce0fb7641de04708870294060cd152f2fc84084a
Michael Vandi <mikevee2013@gmail.com>
2023-11-22 11:08:47 -0800

    Check virtual env activation immediately after activating

--- src/train/run.sh
+++ src/train/run.sh
@@ -34,37 +34,34 @@ python3 -m venv .venv
 echo "Activating the virtual environment"
 source .venv/bin/activate
 
+# Confirming Virtual Environment is active
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Virtual Environment is active"
+else 
+    echo "Virtual Environment is not active. Please activate it."
+    exit 1
+fi
+
 # Changing into server/src directory
 echo "Changing into server/src directory"
 cd server/src
 
-# Confirming Virtual Environment is active
-if [[ "$VIRTUAL_ENV" != "" ]]; then
-    echo "Virtual Environment is active"
-    
-    # If requirements.txt exists in the directory
-    if [ -e requirements.txt ]; then
+# If requirements.txt exists in the directory
+if [ -e requirements.txt ]; then
 
-        # Pip Install 
-        echo "Installing requirements from requirements.txt"
-        pip install -r requirements.txt
-    
-        # If app.py exists in the directory, run it
-        if [ -e app.py ]; then
-            echo "Running app.py"
-            python app.py
+    # Pip Install 
+    echo "Installing requirements from requirements.txt"
+    pip install -r requirements.txt
 
-        else
-            echo "app.py file does not exist in the current directory"
-        fi
+    # If app.py exists in the directory, run it
+    if [ -e app.py ]; then
+        echo "Running app.py"
+        python app.py
 
     else
-        echo "requirements.txt file does not exist in the current directory"
+        echo "app.py file does not exist in the current directory"
     fi
-else 
-    echo "Virtual Environment is not active. Please activate it."
+
+else
+    echo "requirements.txt file does not exist in the current directory"
 fi
-
-
-
-
