7b19e4991390e7bf1626e56920a4f34a4ea9128e
Charles Karpati <charles.karpati@gmail.com>
2023-11-29 18:58:25 -0500

    update docs

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-docs_dir="./docs"
-docs_temp_dir="./documentation"
-swap_dir="./temp_swap_dir"
-
 # Function to build and replace 'docs'
 build_and_replace_docs() {
   # Create the documenation using chatGPT
   # node mkdocs.js
   mkdocs build
-  rm -r "$docs_dir"
-  mv "site" "$docs_dir"
+  # Backup current 'docs' and move built site to 'docs'
+  mv "./docs" "./docs_backup"
+  mv "./site" "./docs"
 }
 
 # Check if '/docs' directory exists and has '.md' files
 if [ -d "$docs_dir" ] && [ "$(find "$docs_dir" -name '*.md' | wc -l)" -gt 0 ]; then
-  # Swap 'docs' and 'docs_temp' directories
-  mv "$docs_dir" "$swap_dir"
-  mv "$docs_temp_dir" "$docs_dir"
-  mv "$swap_dir" "$docs_temp_dir"
-  echo "Swapped '$docs_dir' and '$docs_temp_dir' directories."
+  # Swap 'docs' and 'documentation' directories
+  mv "./docs" "./temp_swap_dir"
+  mv "./documentation" "./docs"
+  mv "./temp_swap_dir" "./documentation"
+  echo "Swapped "./docs" and "./documentation" directories."
 fi
 
 # Build and replace 'docs' regardless of whether a swap happened or not
