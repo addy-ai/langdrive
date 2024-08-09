e488ba6b246aa7a256c341c247ffa959abfc2ea8
Michael Vandi <mikevee2013@gmail.com>
2024-08-08 17:32:10 -0700

    Changed inference start script back to sh

--- src/inference/start.sh
+++ src/inference/start.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+
+# Activate the virtual environment
+. .venv/bin/activate 
 
 # Start the inference server
-source .venv/bin/activate # Activate the virtual environment
-
-# Start the inference server
-nohup python3 -m app
+nohup python3 -m app &
