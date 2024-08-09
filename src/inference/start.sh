a2d057fea41860ea360c21ce61cd32f968bd1997
Michael Vandi <mikevee2013@gmail.com>
2024-08-08 17:20:41 -0700

    update to .venv

--- src/inference/start.sh
+++ src/inference/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the inference server
-source ./venv/bin/activate # Activate the virtual environment
+source .venv/bin/activate # Activate the virtual environment
 
 # Start the inference server
 nohup python3 -m app
