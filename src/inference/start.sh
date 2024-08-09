3e46b29ff9f2afa2ea2c8f645e18c6563ed386a5
Michael Vandi <mikevee2013@gmail.com>
2024-08-08 17:14:30 -0700

    changed inference shebang to bash

--- src/inference/start.sh
+++ src/inference/start.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Start the inference server
 source ./venv/bin/activate # Activate the virtual environment
 
 # Start the inference server
-nohup python3 -m app
+nohup python3 -m app &
